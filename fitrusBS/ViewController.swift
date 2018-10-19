74e37a9624316ed4e391795eb208700760ace581
jonghyun <wjh0389@gmail.com>
2018-10-19 13:56:40 +0900

    Initial Commit

---
+++ fitrusBS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitrusBS
+//
+//  Created by 우종현 on 2018. 10. 19..
+//  Copyright © 2018년 jonghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
